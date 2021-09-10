9f4c90cc23b83f7fe00213674a303c01c2491ab2
Kate Shauliuk <Katsiarynabaranava@gmail.com>
2021-09-10 21:52:39 +0300

    Second commit

--- src/by/Shauliuk/tasks/Main.java
+++ src/by/Shauliuk/tasks/Main.java
@@ -1,7 +1,7 @@
 package by.Shauliuk.tasks;
 
 public class Main {
-    public static void main(String[] args) {
+/*    public static void main(String[] args) {
         Tester.testInt();
         Tester.testByte();
         Tester.testShort();
@@ -10,5 +10,15 @@ public class Main {
         Tester.testChar();
         Tester.testFloat();
         Tester.testBoolean();
+    }
+}*/
+
+    public static void main(String[] args) {
+        int a = 0;
+        for (int i = 2; i <= 20; i++) {
+            if (i % 2 == 0) a++;
+        }
+        int[] Mas = new int[a];
+        System.out.print(Mas[a] + " ");
     }
 }
